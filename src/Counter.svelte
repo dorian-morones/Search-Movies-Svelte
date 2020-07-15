a894ca653cd8bc3942c2766973ab604d124bcf09
dorianmorones <dorian@kinedu.com>
2020-07-14 23:16:14 -0700

    config project and first steps

---
+++ src/Counter.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let counter = 0;
+
+	function handleClick () {
+		counter ++;
+  }
+  
+  $: numValidation = counter % 2 === 0 ? 'is even' : 'is odd';
+</script>
+
+<div>
+	<button on:click={handleClick}>Incrementar</button>
+	<p>{counter} = {numValidation}</p>
+</div>
+
+<style>
+
+</style>
